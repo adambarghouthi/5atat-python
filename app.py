562f82ce3a5528d711a4a385746b8ca167824875
Adam Albarghouthi <adambargh@gmail.com>
2024-02-15 16:38:58 -0500

    minor

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import re
-from flask import Flask, request
+import json
+from flask import Flask, jsonify, request
 from openai import OpenAI
 
 app = Flask(__name__)
@@ -10,9 +11,22 @@ openai_client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
+class Product:
+    def __init__(self, name, description, price, sizes, colours, active, images):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.sizes = sizes
+        self.colours = colours
+        self.active = active
+        self.images = images
+
 @app.route('/', methods=["GET"])
 def hello_world():
-    return '5atat Python Functions'
+    return jsonify({
+        'status': 200,
+        'message': 'Welcome to 5atat Python Functions'
+    })
 
 @app.route('/get-insta-products', methods=["GET"])
 def get_insta_products():
@@ -20,31 +34,69 @@ def get_insta_products():
     insta_user = args.get("insta_user")
 
     if not insta_user:
-        abort(400, 'insta_user is empty')
+        return jsonify({
+            'status': 400,
+            'message': 'insta_user cannot be empty.'
+        })
 
-    return "insta user: " + insta_user
+    # Get instance
+    L = instaloader.Instaloader(
+        quiet=True,
+        download_pictures=False,
+        download_videos=False,
+        download_video_thumbnails=False,
+        save_metadata=False,
+        compress_json=False,
+        post_metadata_txt_pattern=""
+    )
+
+    L.login("5atatapp", "comdem-0figka-caXdob")
+    print("Logged in as 5atatapp")
+
+    profile = instaloader.Profile.from_username(L.context, insta_user)
+
+    products = []
+    count = 0
+
+    for post in profile.get_posts():
+        if post.is_video:
+            continue
+
+        media = []
+
+        if post.mediacount > 1:
+            sidecar_nodes = post.get_sidecar_nodes()
+            for (node in sidecar_nodes):
+                if not node.is_video:
+                    media.append(node.url)
+        else:
+            media.append(post.url)
+
+        caption = re.sub(r"[#|@]([a-zA-Z0-9-_.]+)", "", post.caption)
+        response = openai_client.chat.completions.create(
+            model="gpt-4-turbo-preview",
+            response_format={"type": "json_object"},
+            messages=[
+                {"role": "system", "content": "You are given a product description. You only have to extract the following: name, description, price, sizes, colours, and active. Just use what is given. Price is a number. Make it 0 if not found. Sizes and colours are empty arrays or arrays of strings. Active is a boolean. Make false if product is not available. Spell colours with 'ou' not 'o'. Output in a JSON object."},
+                {"role": "user", "content": caption}
+            ]
+        )
+        json_response = json.loads(response.choices[0].message.content)
+
+        products.append(Product(
+            name=json_response.name,
+            description=json_response.description,
+            price=json_response.price,
+            sizes=json_response.sizes,
+            colours=json_response.colours,
+            active=json_response.active,
+            images=media
+        ))
+
+        count += 1
     
-    # # Get instance
-    # L = instaloader.Instaloader(
-    #     quiet=True,
-    #     download_pictures=False,
-    #     download_videos=False,
-    #     download_video_thumbnails=False,
-    #     save_metadata=False,
-    #     compress_json=False,
-    #     post_metadata_txt_pattern=""
-    # )
+    return jsonify({
+        'status': 200,
+        'products': products
+    })
 
-    # L.login("5atatapp", "comdem-0figka-caXdob")
-    # print("Logged in as 5atatapp")
-
-    # profile = instaloader.Profile.from_username(L.context, insta_user)
-
-    # for post in profile.get_posts():
-    # media = []
-    # if post.mediacount > 1:
-        
-    #     sidecar_nodes = post.get_sidecar_nodes()
-    #     for (node in sidecar_nodes):
-    #         if not node.is_video:
-            
