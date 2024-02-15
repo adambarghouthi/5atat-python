28330d83143c7e43b50a722abae1454a1f6348cc
Adam Albarghouthi <adambargh@gmail.com>
2024-02-15 15:55:18 -0500

    minor

--- app.py
+++ app.py
@@ -18,11 +18,33 @@ def hello_world():
 def get_insta_products():
     args = request.args
     insta_user = args.get("insta_user")
-    return "instagram user: " + insta_user
-    # # Get instance
-    # L = instaloader.Instaloader(quiet=True, download_pictures=False, download_videos=False, download_video_thumbnails=False, save_metadata=False, compress_json=False, post_metadata_txt_pattern="")
-    # L.login("5atatapp", "comdem-0figka-caXdob")
 
+    if not insta_user:
+        abort(400, 'insta_user is empty')
+
+    return "insta user: " + insta_user
+    
+    # # Get instance
+    # L = instaloader.Instaloader(
+    #     quiet=True,
+    #     download_pictures=False,
+    #     download_videos=False,
+    #     download_video_thumbnails=False,
+    #     save_metadata=False,
+    #     compress_json=False,
+    #     post_metadata_txt_pattern=""
+    # )
+
+    # L.login("5atatapp", "comdem-0figka-caXdob")
     # print("Logged in as 5atatapp")
 
     # profile = instaloader.Profile.from_username(L.context, insta_user)
+
+    # for post in profile.get_posts():
+    # media = []
+    # if post.mediacount > 1:
+        
+    #     sidecar_nodes = post.get_sidecar_nodes()
+    #     for (node in sidecar_nodes):
+    #         if not node.is_video:
+            
